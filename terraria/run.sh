3df54fe2e8dd0b4f68e284dcf4f57075c8997445
Anthony Neto <aneto@denirostaff.com>
2017-05-02 14:57:41 -0700

    updating terraria to 1353

--- terraria/run.sh
+++ terraria/run.sh
@@ -2,5 +2,5 @@
 
 [ ! -f /data/config.txt ] && cp /config.txt.default /data/config.txt
 
-exec tail -f /dev/null | /src/Linux/TerrariaServer.bin.x86_64 \
+exec tail -f /dev/null | /src/${TERRARIA_VERSION}/Linux/TerrariaServer.bin.x86_64 \
   -config /data/config.txt
