fb1d78b1529e21256a1a80a1aedd592c39d4b98b
Chase Bolt <chase.bolt@gmail.com>
2017-05-03 12:03:58 -0700

    fix locale issue

--- homeseer/run.sh
+++ homeseer/run.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
-export LANG=en_US.UTF-8
-
-TIMEZONE=${TIMEZONE:-"America/Los_Angeles"}
-echo $TIMEZONE > /etc/timezone
-dpkg-reconfigure -f noninteractive tzdata
 
 # bug fix for case sensitive filesystems
 # without this myhs.homeseer.com wont load icons
