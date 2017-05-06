786682b8107fb7dfcdf2b0b2ece13024f44d43e1
Anthony Neto <aneto@denirostaff.com>
2017-05-05 18:27:08 -0700

    dont error if config is mounted

--- squid/run.sh
+++ squid/run.sh
@@ -4,8 +4,8 @@ set -euo pipefail
 export CACHE_SIZE_MEM=${CACHE_SIZE_MEM:-"1024"}
 export CACHE_SIZE_DISK=${CACHE_SIZE_DISK:-"1024"}
 
-sed -i "s/%%CACHE_SIZE_MEM%%/${CACHE_SIZE_MEM}/" /etc/squid/squid.conf
-sed -i "s/%%CACHE_SIZE_DISK%%/${CACHE_SIZE_DISK}/" /etc/squid/squid.conf
+sed -i "s/%%CACHE_SIZE_MEM%%/${CACHE_SIZE_MEM}/" /etc/squid/squid.conf || true
+sed -i "s/%%CACHE_SIZE_DISK%%/${CACHE_SIZE_DISK}/" /etc/squid/squid.conf || true
 
 if [ ! -d /var/cache/squid/00 ]; then
   echo "Initializing Object cache..."
