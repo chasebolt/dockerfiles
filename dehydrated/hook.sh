5c67abac23467d5a7f5e1ef929ed4a307ef908d6
Chase Bolt <chase.bolt@gmail.com>
2019-08-20 10:19:54 -0700

    add new sync_cert hook

--- dehydrated/hook.sh
+++ dehydrated/hook.sh
@@ -58,6 +58,32 @@ clean_challenge() {
   # The parameters are the same as for deploy_challenge.
 }
 
+sync_cert() {
+  local KEYFILE="${1}" CERTFILE="${2}" FULLCHAINFILE="${3}" CHAINFILE="${4}" REQUESTFILE="${5}"
+
+  # This hook is called after the certificates have been created but before
+  # they are symlinked. This allows you to sync the files to disk to prevent
+  # creating a symlink to empty files on unexpected system crashes.
+  #
+  # This hook is not intended to be used for further processing of certificate
+  # files, see deploy_cert for that.
+  #
+  # Parameters:
+  # - KEYFILE
+  #   The path of the file containing the private key.
+  # - CERTFILE
+  #   The path of the file containing the signed certificate.
+  # - FULLCHAINFILE
+  #   The path of the file containing the full certificate chain.
+  # - CHAINFILE
+  #   The path of the file containing the intermediate certificate(s).
+  # - REQUESTFILE
+  #   The path of the file containing the certificate signing request.
+
+  # Simple example: sync the files before symlinking them
+  # sync "${KEYFILE}" "${CERTFILE} "${FULLCHAINFILE}" "${CHAINFILE}" "${REQUESTFILE}"
+}
+
 deploy_cert() {
   DOMAIN="${1}"
   KEYFILE="${2}"
@@ -148,6 +174,31 @@ request_failure() {
   #   The request type used (head/get/post)
 }
 
+generate_csr() {
+  local DOMAIN="${1}" CERTDIR="${2}" ALTNAMES="${3}"
+
+  # This hook is called before any certificate signing operation takes place.
+  # It can be used to generate or fetch a certificate signing request with external
+  # tools.
+  # The output should be just the cerificate signing request formatted as PEM.
+  #
+  # Parameters:
+  # - DOMAIN
+  #   The primary domain as specified in domains.txt. This does not need to
+  #   match with the domains in the CSR, it's basically just the directory name.
+  # - CERTDIR
+  #   Certificate output directory for this particular certificate. Can be used
+  #   for storing additional files.
+  # - ALTNAMES
+  #   All domain names for the current certificate as specified in domains.txt.
+  #   Again, this doesn't need to match with the CSR, it's just there for convenience.
+
+  # Simple example: Look for pre-generated CSRs
+  # if [ -e "${CERTDIR}/pre-generated.csr" ]; then
+  #   cat "${CERTDIR}/pre-generated.csr"
+  # fi
+}
+
 startup_hook() {
   # This hook is called before the cron command to do some initial tasks
   # (e.g. starting a webserver).
@@ -193,4 +244,7 @@ create_haproxy() {
   cat "${KEYFILE}" "${FULLCHAINFILE}" > "${HAPROXY_DIR}/${DOMAIN}.pem"
 }
 
-HANDLER=$1; shift; $HANDLER "$@"
+HANDLER=$1; shift
+if [[ "${HANDLER}" =~ ^(deploy_challenge|clean_challenge|sync_cert|deploy_cert|deploy_ocsp|unchanged_cert|invalid_challenge|request_failure|generate_csr|startup_hook|exit_hook)$ ]]; then
+  "$HANDLER" "$@"
+fi
