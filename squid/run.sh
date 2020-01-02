2d188a1fe1e38297c4fc311a695c19bba8092d11
Chase Bolt <chase.bolt@gmail.com>
2020-01-02 14:30:35 -0800

    wip

--- squid/run.sh
+++ squid/run.sh
@@ -10,13 +10,8 @@ sed -i "s/%%CACHE_SIZE_DISK%%/${CACHE_SIZE_DISK}/" /etc/squid/squid.conf || true
 if [ ! -d /var/cache/squid/00 ]; then
   echo "Initializing Object cache..."
   chown squid:squid /var/cache/squid
-  /usr/sbin/squid -f /etc/squid/squid.conf -N -z
+  /usr/sbin/squid -f /etc/squid/squid.conf -Nz
   chown -R squid:squid /var/cache/squid
 fi
-if [ ! -d /var/lib/ssl_db ]; then
-  echo "Initializing SSL cache..."
-  /usr/lib/squid/ssl_crtd -c -s /var/lib/ssl_db
-  chown -R squid:squid /var/lib/ssl_db
-fi
 
-/usr/sbin/squid -f /etc/squid/squid.conf -NYd 1
+/usr/sbin/squid -f /etc/squid/squid.conf -SNYd 1
